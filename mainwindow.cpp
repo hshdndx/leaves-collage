87e53ead1d326c5f8110910c275f0c2ae7d025b4
hshdndx <hanshuang_dndx@163.com>
2014-04-28 17:48:54 +0800

    Fix bug with link

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,48 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QtGui>
+#include <QPalette>
+#include "loginwindow.h"
+#include "regwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setWindowTitle(tr("Welcome"));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+/*QUrl::QUrl ( const QString & url )
+{
+     url = new QUrl(QString("http://wendycan.org"));
+}
+
+QUrl::~QUrl ()
+{
+    delete url;
+}*/
+
+bool MainWindow::openwebpage()
+{
+  QDesktopServices::openUrl(QUrl(QLatin1String("http://wendycan.org")));
+}
+
+void MainWindow::createDialog1()
+{
+     LoginWindow *signdialog= new LoginWindow;
+     signdialog->show();
+}
+
+void MainWindow::createDialog2()
+{
+    RegisterWindow *regdialog = new RegisterWindow;
+    regdialog->show();
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,15 @@
 
 #include <QMainWindow>
 
+#include <QDialog>
+#include <QtGui/QtGui>
+#include <QUrl>
+#include <QDesktopServices>
+
+class QLabel;
+class QPushButton;
+class QUrl;
+
 namespace Ui {
 class MainWindow;
 }
@@ -15,8 +24,37 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+public:
+    QLabel *label;
+    QPushButton *loginButton;
+    QPushButton *regButton;
+    QPushButton *findButton;
+    QPushButton *jigsawButton;
+    QPushButton *closeButton;
+
+//    QUrl *url;
+
+signals:
+    void on_pushButton_3_clicked();
+    void on_findButton_clicked();
+    void on_loginButton_clicked();
+    void on_regButton_clicked();
+//    void findleaves();
+//    void jigsaw();
+
 private:
     Ui::MainWindow *ui;
+
+
+public  slots:
+//   void loginClicked();
+//   void regClicked();
+//   void leaveClicked();
+//   void jigsawClicked();
+//     bool QDesktopServices::openUrl ( const QUrl & url );
+    bool openwebpage();
+    void createDialog1();
+    void createDialog2();
 };
 
 #endif // MAINWINDOW_H
